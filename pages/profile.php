9e7a9e12edc855a3fb851501a173731d3ec69d67
sarah <109282676+salehuddn@users.noreply.github.com>
2023-07-12 22:48:40 +0800

    profle page - view & edit fxn

--- pages/profile.php
+++ pages/profile.php
@@ -16,8 +16,52 @@
     $row = mysqli_fetch_assoc($result);
     $name = $row['name'];
     $email = $row['email'];
+    $address = $row['address'];
+    $city = $row['city'];
+    $state = $row['state'];
+    $zip_code = $row['zip_code'];
   } else {
     echo "Error";
+  }
+
+  if (isset($_POST['profile'])) {
+    $newName = $_POST['userName'];
+    $newEmail = $_POST['userEmail'];
+  
+    // Update the user's profile information
+    $query = "UPDATE users SET name = '$newName', email = '$newEmail' WHERE id = $userId";
+    $updateResult = mysqli_query($connection, $query);
+  
+    if ($updateResult) {
+      // Update successful
+      $name = $newName;
+      $email = $newEmail;
+      $profileSuccess = "Profile updated successfully.";
+    } else {
+      $editProfileError = "Error updating profile.";
+    }
+  }
+  
+  if (isset($_POST['address'])) {
+    $newAddress = $_POST['userAddress'];
+    $newCity = $_POST['userCity'];
+    $newState = $_POST['userState'];
+    $newZipCode = $_POST['userZipCode'];
+  
+    // Update the user's address information
+    $query = "UPDATE users SET address = '$newAddress', city = '$newCity', state = '$newState', zip_code = '$newZipCode' WHERE id = $userId";
+    $updateResult = mysqli_query($connection, $query);
+  
+    if ($updateResult) {
+      // Update successful
+      $address = $newAddress;
+      $city = $newCity;
+      $state = $newState;
+      $zip_code = $newZipCode;
+      $addressSuccess = "Address updated successfully.";
+    } else {
+      $editAddressError = "Error updating address.";
+    }
   }
 ?>
 
@@ -42,25 +86,82 @@
   <div class="container-fluid">
     <div class="container my-4">
       <div class="row justify-content-center my-4">
-        <div class="col-md-8">
-          <div class="card">
-            <div class="card-header fw-bold text-center">Profile</div>
-            <div class="card-body">
-              <table class="table">
-                <tbody>
-                  <tr>
-                    <th scope="row">Name</th>
-                    <td><?php echo $name; ?></td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Email</th>
-                    <td><?php echo $email; ?></td>
-                  </tr>
-                </tbody>
-              </table>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header fw-bold text-center">Profile</div>
+              <div class="card-body">
+
+                <?php if (isset($profileSuccess)): ?>
+                  <div class="alert alert-success"><?php echo $profileSuccess; ?></div>
+                <?php endif; ?>
+                <?php if (isset($editProfileError)): ?>
+                  <div class="alert alert-danger"><?php echo $editProfileError; ?></div>
+                <?php endif; ?>
+
+                <form method="post" id="profile-form">
+                  <?php if (isset($editProfileError)): ?>
+                  <div class="alert alert-danger"><?php echo $editProfileError; ?></div>
+                  <?php endif; ?>
+                  
+                  <div class="mb-3">
+                    <label for="name" class="form-label">Name: </label>
+                    <input type="text" class="form-control" id="userName" name="userName" value="<?php echo $name; ?>">
+                  </div>
+                  <div class="mb-3">
+                    <label for="email" class="form-label">Email: </label>
+                    <input type="email" class="form-control" id="userEmail" name="userEmail" value="<?php echo $email; ?>">
+                  </div>
+                  <div class="mb-3">
+                    <label for="password" class="form-label">Password: </label>
+                    <input type="password" class="form-control" id="userPassword" name="userPassword">
+                  </div>
+
+                  <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-success" name="profile">Save</button>
+                  </div>
+                </form>
+
+              </div>
             </div>
           </div>
-        </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-header fw-bold text-center">Billing Address</div>
+              <div class="card-body">
+
+                <?php if (isset($addressSuccess)): ?>
+                  <div class="alert alert-success"><?php echo $addressSuccess; ?></div>
+                <?php endif; ?>
+                <?php if (isset($editAddressError)): ?>
+                  <div class="alert alert-danger"><?php echo $editAddressError; ?></div>
+                <?php endif; ?>
+
+                <form method="post" id="address-form">
+                  <div class="mb-3">
+                    <label for="address" class="form-label">Address: </label>
+                    <input type="text" class="form-control" id="userAddress" name="userAddress" value="<?php echo $address ?? ''; ?>">
+                  </div>
+                  <div class="mb-3">
+                    <label for="city" class="form-label">City: </label>
+                    <input type="text" class="form-control" id="userCity" name="userCity" value="<?php echo $city ?? ''; ?>">
+                  </div>
+                  <div class="mb-3">
+                    <label for="state" class="form-label">State: </label>
+                    <input type="text" class="form-control" id="userState" name="userState" value="<?php echo $state ?? ''; ?>">
+                  </div>
+                  <div class="mb-3">
+                    <label for="zipcode" class="form-label">Zip Code: </label>
+                    <input type="text" class="form-control" id="userZipCode" name="userZipCode" value="<?php echo $zip_code ?? ''; ?>">
+                  </div>
+
+                  <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-success" name="address">Save</button>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+          </div>
       </div>
     </div>
 
