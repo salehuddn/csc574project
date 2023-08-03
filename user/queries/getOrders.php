e86e9bd86be7d707f3b64214835c5511ef1173a2
sarah <109282676+salehuddn@users.noreply.github.com>
2023-08-03 23:00:21 +0800

    make search bar in orders page function

--- user/queries/getOrders.php
+++ user/queries/getOrders.php
@@ -3,6 +3,7 @@
 require_once('../../config/connection.php');
 
 $user_id = $_SESSION['userId'];
+$search_query = isset($_GET['search_query']) ? mysqli_real_escape_string($connection, $_GET['search_query']) : '';
 
 $query = "SELECT a.*, b.*, c.name AS 'product_name', (SELECT image_path FROM product_images WHERE product_id = c.id LIMIT 1) AS image_path
           FROM orders a
@@ -10,6 +11,12 @@ $query = "SELECT a.*, b.*, c.name AS 'product_name', (SELECT image_path FROM pro
           INNER JOIN products c ON b.product_id = c.id
           INNER JOIN users d ON a.user_id = d.id
           WHERE a.user_id = $user_id";
+
+// Add search condition if a search query is provided
+if (!empty($search_query)) {
+  $query .= " AND (c.name LIKE '%$search_query%' OR a.id LIKE '%$search_query%')";
+}
+
 $result = mysqli_query($connection, $query);
 
 if ($stmt = mysqli_prepare($connection, $query)) {
@@ -19,4 +26,11 @@ if ($stmt = mysqli_prepare($connection, $query)) {
   $totalOrders = mysqli_num_rows($result);
 } else {
   echo "Error";
+}
+
+// Handle the reset action
+if (isset($_GET['reset']) && $_GET['reset'] === 'true') {
+  // Clear the search query
+  header("Location: ../../user/orders/view.php");
+  exit();
 }
